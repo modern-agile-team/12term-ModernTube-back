1c9e46c2adf0b539254045d0418bc62b80e3a62e
lee0802-120 <lpk3782@gmail.com>
2026-08-12 15:45:14 +0900

    style: 안쓰는 import와 가독성을 위한 코드 위치 재배열 수행.

--- src/main/java/com/moderntube/moderntubo_backend/controller/common/VideoController.java
+++ src/main/java/com/moderntube/moderntubo_backend/controller/common/VideoController.java
@@ -11,15 +11,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/videos")
@@ -36,7 +32,8 @@ public class VideoController {
     public ResponseEntity<?> uploadVideo(
             @Parameter(description = "업로드할 동영상 파일", required = true)
             @RequestParam("video") MultipartFile file,
-            @CurrentUser CustomUserDetails currentUser) {
+            @CurrentUser CustomUserDetails currentUser
+    ) {
         VideoUploadResponse response = videoService.uploadVideo(file, currentUser);
         return ResponseEntity.ok(new ApiResponse(true, response));
     }
